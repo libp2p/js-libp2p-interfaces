1d0cc68645267f516c54a19d65356d94433acb86
David Dias <daviddias.p@gmail.com>
2015-07-10 22:02:57 -0700

    base tests

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,4 @@
 module.exports = function (test, common) {
   require('./base-test.js').all(test, common)
+  require('./stress-test.js').all(test, common)
 }
--- tests/stress-test.js
+++ tests/stress-test.js
@@ -1,3 +1,5 @@
+/* knobs - nStreams, nMsg, sizeMsg[low, high] */
+
 var streamPair = require('stream-pair')
 
 module.exports.all = function (test, common) {
@@ -20,29 +22,6 @@ module.exports.all = function (test, common) {
       })
 
       connListener.on('stream', function (stream) {
-        t.pass('got stream')
-      })
-    })
-  })
-
-  test('Open a stream from the listener', function (t) {
-    common.setup(test, function (err, Muxer) {
-      t.plan(4)
-      t.ifError(err, 'Should not throw')
-
-      var pair = streamPair.create()
-      var dialer = new Muxer()
-      var listener = new Muxer()
-
-      var connDialer = dialer.attach(pair)
-      var connListener = listener.attach(pair.other)
-
-      connListener.dialStream(function (err, stream) {
-        t.ifError(err, 'Should not throw')
-        t.pass('dialed stream')
-      })
-
-      connDialer.on('stream', function (stream) {
         t.pass('got stream')
       })
     })
