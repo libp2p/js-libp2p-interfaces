a454989cf83e62f38b4206e3c9d9a3c110f592b6
David Dias <daviddias.p@gmail.com>
2015-07-22 13:00:10 -0700

    add timed-tape

--- tests/base-test.js
+++ tests/base-test.js
@@ -80,6 +80,51 @@ module.exports.all = function (test, common) {
     })
   })
 
+  test('Open a stream on one side, write, open a stream in the other side', function (t) {
+    common.setup(test, function (err, Muxer) {
+      t.plan(9)
+      t.ifError(err, 'Should not throw')
+
+      var pair = streamPair.create()
+      var dialer = new Muxer()
+      var listener = new Muxer()
+
+      var connDialer = dialer.attach(pair, false)
+      var connListener = listener.attach(pair.other, true)
+
+      connDialer.dialStream(function (err, stream) {
+        t.ifError(err, 'Should not throw')
+        t.pass('dialed stream from dialer')
+
+        stream.write('hey')
+      })
+
+      connListener.on('stream', function (stream) {
+        t.pass('listener got stream')
+
+        stream.on('data', function (chunk) {
+          t.equal(chunk.toString(), 'hey')
+        })
+
+        connListener.dialStream(function (err, stream) {
+          t.ifError(err, 'Should not throw')
+          t.pass('dialed stream from listener')
+
+          stream.write('hello')
+        })
+
+      })
+
+      connDialer.on('stream', function (stream) {
+        t.pass('dialer got stream')
+
+        stream.on('data', function (chunk) {
+          t.equal(chunk.toString(), 'hello')
+        })
+      })
+    })
+  })
+
   test('Open a stream using the net.connect pattern', function (t) {
     common.setup(test, function (err, Muxer) {
       t.plan(3)
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,7 @@
+var timed = require('timed-tape')
+
 module.exports = function (test, common, mega) {
+  test = timed(test)
   require('./base-test.js').all(test, common)
   require('./stress-test.js').all(test, common)
   if (mega) {
