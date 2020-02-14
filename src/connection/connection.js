0dd4d65d6e2762ab7bde70e86a9bb95373ee8c6b
achingbrain <alex@achingbrain.net>
2020-02-14 10:44:29 +0000

    chore: pr comments

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -6,6 +6,52 @@ const withIs = require('class-is')
 const errCode = require('err-code')
 const Status = require('./status')
 
+function validateArgs (localAddr, localPeer, remotePeer, newStream, close, getStreams, stat) {
+  if (localAddr && !multiaddr.isMultiaddr(localAddr)) {
+    throw errCode(new Error('localAddr must be an instance of multiaddr'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (!PeerId.isPeerId(localPeer)) {
+    throw errCode(new Error('localPeer must be an instance of peer-id'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (!PeerId.isPeerId(remotePeer)) {
+    throw errCode(new Error('remotePeer must be an instance of peer-id'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (typeof newStream !== 'function') {
+    throw errCode(new Error('new stream must be a function'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (typeof close !== 'function') {
+    throw errCode(new Error('close must be a function'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (typeof getStreams !== 'function') {
+    throw errCode(new Error('getStreams must be a function'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (!stat) {
+    throw errCode(new Error('connection metadata object must be provided'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (stat.direction !== 'inbound' && stat.direction !== 'outbound') {
+    throw errCode(new Error('direction must be "inbound" or "outbound"'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (!stat.timeline) {
+    throw errCode(new Error('connection timeline object must be provided in the stat object'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (!stat.timeline.open) {
+    throw errCode(new Error('connection open timestamp must be provided'), 'ERR_INVALID_PARAMETERS')
+  }
+
+  if (!stat.timeline.upgraded) {
+    throw errCode(new Error('connection upgraded timestamp must be provided'), 'ERR_INVALID_PARAMETERS')
+  }
+}
+
 /**
  * An implementation of the js-libp2p connection.
  * Any libp2p transport should use an upgrader to return this connection.
@@ -30,49 +76,7 @@ class Connection {
    * @param {string} [properties.stat.encryption] connection encryption method identifier.
    */
   constructor ({ localAddr, remoteAddr, localPeer, remotePeer, newStream, close, getStreams, stat }) {
-    if (localAddr && !multiaddr.isMultiaddr(localAddr)) {
-      throw errCode(new Error('localAddr must be an instance of multiaddr'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (!PeerId.isPeerId(localPeer)) {
-      throw errCode(new Error('localPeer must be an instance of peer-id'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (!PeerId.isPeerId(remotePeer)) {
-      throw errCode(new Error('remotePeer must be an instance of peer-id'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (typeof newStream !== 'function') {
-      throw errCode(new Error('new stream must be a function'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (typeof close !== 'function') {
-      throw errCode(new Error('close must be a function'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (typeof getStreams !== 'function') {
-      throw errCode(new Error('getStreams must be a function'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (!stat) {
-      throw errCode(new Error('connection metadata object must be provided'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (stat.direction !== 'inbound' && stat.direction !== 'outbound') {
-      throw errCode(new Error('direction must be "inbound" or "outbound"'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (!stat.timeline) {
-      throw errCode(new Error('connection timeline object must be provided in the stat object'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (!stat.timeline.open) {
-      throw errCode(new Error('connection open timestamp must be provided'), 'ERR_INVALID_PARAMETERS')
-    }
-
-    if (!stat.timeline.upgraded) {
-      throw errCode(new Error('connection upgraded timestamp must be provided'), 'ERR_INVALID_PARAMETERS')
-    }
+    validateArgs(localAddr, localPeer, remotePeer, newStream, close, getStreams, stat)
 
     /**
      * Connection identifier.
