1379eb6c80bd1ce02cd1fd9ae6091c6e78bee460
Vasco Santos <vasco.santos@moxy.studio>
2020-04-06 20:20:27 +0200

    chore: add tests for peer-discovery

--- src/peer-discovery/tests/index.js
+++ src/peer-discovery/tests/index.js
@@ -1,17 +1,31 @@
 /* eslint-env mocha */
 'use strict'
 
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('dirty-chai'))
+
+const multiaddr = require('multiaddr')
+const PeerId = require('peer-id')
+
+const delay = require('delay')
+const pDefer = require('p-defer')
+
 module.exports = (common) => {
   describe('interface-peer-discovery', () => {
     let discovery
 
-    before(async () => {
+    beforeEach(async () => {
       discovery = await common.setup()
     })
 
     after(() => common.teardown && common.teardown())
 
-    afterEach('ensure discovery was stopped', () => discovery.stop())
+    afterEach('ensure discovery was stopped', async () => {
+      await discovery.stop()
+
+      discovery.removeAllListeners()
+    })
 
     it('can start the service', async () => {
       await discovery.start()
@@ -29,6 +43,50 @@ module.exports = (common) => {
     it('should not fail to start the service if it is already started', async () => {
       await discovery.start()
       await discovery.start()
+    })
+
+    it('should listen a peer event after start', async () => {
+      const defer = pDefer()
+      await discovery.start()
+
+      discovery.once('peer', ({ id, multiaddrs }) => {
+        expect(id).to.exist()
+        expect(PeerId.isPeerId(id)).to.eql(true)
+        expect(multiaddrs).to.exist()
+
+        multiaddrs.forEach((m) => expect(multiaddr.isMultiaddr(m)).to.eql(true))
+
+        defer.resolve()
+      })
+
+      return defer.promise
+    })
+
+    it('should not receive a peer event before start', async () => {
+      discovery.once('peer', () => {
+        throw new Error('should not receive a peer event before start')
+      })
+
+      await delay(2000)
+    })
+
+    it('should not receive a peer event after stop', async () => {
+      const deferStart = pDefer()
+
+      await discovery.start()
+
+      discovery.once('peer', () => {
+        deferStart.resolve()
+      })
+
+      await deferStart.promise
+      await discovery.stop()
+
+      discovery.once('peer', () => {
+        throw new Error('should not receive a peer event after stop')
+      })
+
+      await delay(2000)
     })
   })
 }
