b2cdea0f5418e0fca2dcc815fe0050acc9aa61d7
Vasco Santos <vasco.santos@moxy.studio>
2021-04-21 16:55:21 +0200

    chore: separate type for functions

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,5 @@
-export type DhtSelectors = { [key: string]: function (Uint8Array, Uint8Array[]): number }
-export type DhtValidators = { [key: string]: { func: (key: Uint8Array, value: Uint8Array) => Promise<void> } }
+export type SelectFn = function (Uint8Array, Uint8Array[]): number
+export type ValidateFn = function (Uint8Array, Uint8Array): Promise<void>
+
+export type DhtSelectors = { [key: string]: SelectFn }
+export type DhtValidators = { [key: string]: { func: ValidateFn } }
