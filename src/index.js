72eda9b075533cc8ca22bd372632b17f4fe92931
Vasco Santos <vasco.santos@moxy.studio>
2021-04-22 09:30:45 +0200

    chore: revert types in root index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * @typedef {import('./types').SelectFn} SelectFn
- * @typedef {import('./types').ValidateFn} ValidateFn
- * @typedef {import('./types').DhtSelectors} DhtSelectors
- * @typedef {import('./types').DhtValidators} DhtValidators
- */
