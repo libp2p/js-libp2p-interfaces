4885f306496b8ccb3fccadc936cb74085580785c
achingbrain <alex@achingbrain.net>
2022-02-07 07:48:25 +0000

    fix: add logging formatters

--- packages/libp2p-logger/src/index.ts
+++ packages/libp2p-logger/src/index.ts
@@ -1,4 +1,22 @@
 import debug from 'debug'
+import { base58btc } from 'multiformats/bases/base58'
+import { base32 } from 'multiformats/bases/base32'
+import type { PeerId } from '@libp2p/interfaces/peer-id'
+
+// Add a formatter for converting to a base58 string
+debug.formatters.b = (v: Uint8Array) => {
+  return base58btc.baseEncode(v)
+}
+
+// Add a formatter for converting to a base32 string
+debug.formatters.t = (v: Uint8Array) => {
+  return base32.baseEncode(v)
+}
+
+// Add a formatter for stringifying peer ids
+debug.formatters.p = (p: PeerId) => {
+  return p.toString(base58btc)
+}
 
 export interface Logger {
   (formatter: any, ...args: any[]): void
