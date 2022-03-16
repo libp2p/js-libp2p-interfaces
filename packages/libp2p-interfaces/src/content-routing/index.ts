49da9d5883618a672d64542eee13972e603a5b3d
Alex Potsides <alex@achingbrain.net>
2022-03-16 17:04:00 +0000

    fix: update content routing get return type (#182)
    There is no way of knowing where the result came from when talking to go-ipfs over http so remove the field.

--- packages/libp2p-interfaces/src/content-routing/index.ts
+++ packages/libp2p-interfaces/src/content-routing/index.ts
@@ -1,16 +1,10 @@
 import type { CID } from 'multiformats/cid'
 import type { AbortOptions } from '../index.js'
 import type { PeerData } from '../peer-data/index.js'
-import type { PeerId } from '../peer-id/index.js'
-
-export interface GetResult {
-  from: PeerId
-  val: Uint8Array
-}
 
 export interface ContentRouting {
   provide: (cid: CID, options?: AbortOptions) => Promise<void>
   findProviders: (cid: CID, options?: AbortOptions) => AsyncIterable<PeerData>
   put: (key: Uint8Array, value: Uint8Array, options?: AbortOptions) => Promise<void>
-  get: (key: Uint8Array, options?: AbortOptions) => Promise<GetResult>
+  get: (key: Uint8Array, options?: AbortOptions) => Promise<Uint8Array>
 }
