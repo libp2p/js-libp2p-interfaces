e76c93075dcdba03826d695bdbff31bfa803ba1c
Marin Petrunic <marin.petrunic@gmail.com>
2022-08-04 11:19:05 +0200

    feat!: change connection encryption interface to uint8arraylist

--- packages/interface-connection-encrypter-compliance-tests/src/utils/index.ts
+++ packages/interface-connection-encrypter-compliance-tests/src/utils/index.ts
@@ -2,12 +2,13 @@ import { duplexPair } from 'it-pair/duplex'
 import { Multiaddr } from '@multiformats/multiaddr'
 import type { MultiaddrConnection } from '@libp2p/interface-connection'
 import type { Duplex } from 'it-stream-types'
+import type { Uint8ArrayList } from 'uint8arraylist'
 
-export function createMaConnPair (): [MultiaddrConnection, MultiaddrConnection] {
-  const [local, remote] = duplexPair<Uint8Array>()
+export function createMaConnPair (): [MultiaddrConnection<Uint8ArrayList>, MultiaddrConnection<Uint8ArrayList>] {
+  const [local, remote] = duplexPair<Uint8ArrayList>()
 
-  function duplexToMaConn (duplex: Duplex<Uint8Array>): MultiaddrConnection {
-    const output: MultiaddrConnection = {
+  function duplexToMaConn (duplex: Duplex<Uint8ArrayList>): MultiaddrConnection<Uint8ArrayList> {
+    const output: MultiaddrConnection<Uint8ArrayList> = {
       ...duplex,
       close: async () => {},
       remoteAddr: new Multiaddr('/ip4/127.0.0.1/tcp/4001'),
