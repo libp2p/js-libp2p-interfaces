0b157d5666caaaaa8676265cab3e4b010872ee41
Alex Potsides <alex@achingbrain.net>
2022-10-07 15:57:56 +0100

    feat!: add observed address methods (#269)
    When we observe an address that might be externally dialable (via identify or uPNP NAT holepunching, etc), we need some way of signalling that we've either verified it's externally dialable and that we are safe to advertise it, or that it's not dialable and that we should forget about it.
    
    These methods are necessary to implement autonat.

--- packages/interface-address-manager/src/index.ts
+++ packages/interface-address-manager/src/index.ts
@@ -20,12 +20,27 @@ export interface AddressManager extends EventEmitter<AddressManagerEvents> {
   getAnnounceAddrs: () => Multiaddr[]
 
   /**
-   * Get observed multiaddrs
+   * Get observed multiaddrs - these addresses may not have been confirmed as
+   * publicly dialable yet
    */
   getObservedAddrs: () => Multiaddr[]
 
   /**
-   * Add peer observed addresses
+   * Signal that we have confidence an observed multiaddr is publicly dialable -
+   * this will make it appear in the output of getAddresses()
+   */
+  confirmObservedAddr: (addr: Multiaddr) => void
+
+  /**
+   * Signal that we do not have confidence an observed multiaddr is publicly dialable -
+   * this will remove it from the output of getObservedAddrs()
+   */
+  removeObservedAddr: (addr: Multiaddr) => void
+
+  /**
+   * Add peer observed addresses.  These will then appear in the output of getObservedAddrs
+   * but not getAddresses() until their dialability has been confirmed via a call to
+   * confirmObservedAddr.
    */
   addObservedAddr: (addr: Multiaddr) => void
 
