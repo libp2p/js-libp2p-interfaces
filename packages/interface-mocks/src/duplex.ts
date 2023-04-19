0da7c15f7ce330ab75f03f91a39c14b4d7cf37e2
achingbrain <alex@achingbrain.net>
2023-04-19 08:25:39 +0200

    chore: linting

--- packages/interface-mocks/src/duplex.ts
+++ packages/interface-mocks/src/duplex.ts
@@ -2,9 +2,9 @@ import type { Duplex, Source } from 'it-stream-types'
 
 export function mockDuplex (): Duplex<AsyncGenerator<Uint8Array>, Source<Uint8Array>, Promise<void>> {
   return {
-    source: async function * () {
+    source: (async function * () {
       yield * []
-    }(),
+    }()),
     sink: async () => {}
   }
 }
