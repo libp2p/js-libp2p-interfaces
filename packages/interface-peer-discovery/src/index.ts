7fb554c766207a53885c94f6fa98918f4b5ce541
achingbrain <alex@achingbrain.net>
2023-05-04 17:24:28 +0100

    feat!: rename peer discovery symbol to peerDiscovery
    The export `symbol` from the `@libp2p/interface-peer-discovery` is too
    generic to use with modules that might implement peer discovery and
    something else so it's been renamed to `peerDiscovery`.
    
    Objects with `peerDiscovery` symbol properties getters should return
    `PeerDiscovery` instances instead of `boolean` values.
    
    BREAKING CHANGE: the `symbol` export is now named `peerDiscovery`

--- packages/interface-peer-discovery/src/index.ts
+++ packages/interface-peer-discovery/src/index.ts
@@ -22,24 +22,8 @@ import type { EventEmitter } from '@libp2p/interfaces/events'
  */
 export const peerDiscovery = Symbol.for('@libp2p/peer-discovery')
 
-export const symbol = Symbol.for('@libp2p/peer-discovery')
-
 export interface PeerDiscoveryEvents {
   'peer': CustomEvent<PeerInfo>
 }
 
-export interface PeerDiscovery extends EventEmitter<PeerDiscoveryEvents> {
-  /**
-   * Used to identify the peer discovery mechanism
-   */
-  [Symbol.toStringTag]: string
-
-  /**
-   * Used by the isPeerDiscovery function
-   */
-  [symbol]: true
-}
-
-export function isPeerDiscovery (other: any): other is PeerDiscovery {
-  return other != null && Boolean(other[symbol])
-}
+export type PeerDiscovery = EventEmitter<PeerDiscoveryEvents>
